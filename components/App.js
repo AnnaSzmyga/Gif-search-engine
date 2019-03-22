89b215e51280dacb97b292c21c3c6e6885293e0e
Anna Szmyga <anna.szmyga@o2.pl>
2019-03-22 19:23:48 +0100

    Change AJAX request - remove callback and add promise

--- components/App.js
+++ components/App.js
@@ -13,29 +13,34 @@ App = React.createClass({
         this.setState({
             loading: true
         });
-        this.getGif(searchingText, function(gif) {
-            this.setState({
-                loading: false,
-                gif: gif,
-                searchingText: searchingText
-            });
-        }.bind(this));
-    },
-    getGif: function(searchingText, callback) {
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                var data = JSON.parse(xhr.responseText).data;
+        this.getGif(searchingText)
+            .then(response => {
+                var data = response.data;
                 var gif = {
                     url: data.fixed_width_downsampled_url,
                     sourceUrl: data.url
                 };
-                callback(gif);
-            }
-        };
-        xhr.send();
+                this.setState({
+                    loading: false,
+                    gif: gif,
+                    searchingText: searchingText
+                });
+            })
+    },
+    getGif: function(searchingText) {
+        return new Promise(
+            function(resolve, reject) {
+                var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', url);
+                xhr.onload = function() {
+                    if (xhr.status === 200) {
+                        var response = JSON.parse(xhr.responseText);
+                        resolve(response);
+                    }
+                }
+                xhr.send();
+            });
     },
     render: function() {
         var styles = {
